baeac374429e7599f1670e5b3a7aa1939ffc7769
GLENN SLADE <glennslade6@gmail.com>
2021-07-16 12:22:50 +0100

    Test of Procedure in tidyverse for merging table data

---
+++ Join_tables.R
@@ -0,0 +1,14 @@
+library (tidyverse)    
+Plot_data <- read_csv ("C:/Workspace/Reproducibility/Plot_Data.csv") # Plot Species, AGB and coordinate data
+Survey_data <- read_csv ("C:/Workspace/Reproducibility/Survey_Data.csv") # Survey Date, Time, Wind and illumination Conditions
+SfM_data <- read_csv ("C:/Workspace/Reproducibility/SfM_CH_Data.csv") # Reconstructed Canopy Height Metrics for each plot calculated from .las files from each survey
+as_tibble(Plot_data) #turns data frame in tidy tibble
+view (Plot_data)
+as_tibble(Survey_data) #turns data frame in tidy tibble
+view (Survey_data)
+as_tibble(SfM_data) #turns data frame in tidy tibble
+view (SfM_data)
+SfM_Plot_data <- left_join(Plot_data,SfM_data,by = "PlotID") # Merges Plot and SfM Data using Plot ID
+Survey_SfM_Plot_data <- left_join(SfM_Plot_data,Survey_data,by = "SurveyID") # Merges Survey and SfM_Plot Data using Survey ID
+R_data <- Survey_SfM_Plot_data # Final data set named R_data to be used in all subsequent analysis
+write.csv(R_data,"C:/Workspace/Reproducibility/R_data.csv")
