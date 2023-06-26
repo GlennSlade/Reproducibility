2b2ec83813e05bbc3c32c9e0184caa65e7d516d0
GlennSlade <glennslade6@gmail.com>
2023-06-26 08:56:34 +0100

    updates

--- HAG/CHM_plots_v3.R
+++ HAG/CHM_plots_v3.R
@@ -22,7 +22,7 @@ library(gridExtra)
 library(DescTools)
 library(sf)
 library(exactextractr)
-library(writexl) 
+lifor
 library (raster)
 library(dplyr)
 library(remotes)
@@ -41,7 +41,7 @@ tic()
 
 #Read in Survey 
 
-for (x in 2:3) {
+for (x in 4:10) {
 
 S_laz = readLAS(paste0("/raid/home/gs558/share/Reproducibility/Processed//LAZ/S",x,"_dpc_export.laz"))
 
