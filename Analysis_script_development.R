ab5c6931e9e62c630dc1208166267ce25362fba8
GLENN SLADE <glennslade6@gmail.com>
2023-06-26 17:47:33 +0100

    update

--- Analysis_script_development.R
+++ Analysis_script_development.R
@@ -25,7 +25,7 @@ master_df <- full_join(survey_df,plot, by = "plot")
 #save the master_df for use in other scripts
 write_xlsx(master_df,"C:/Workspace/R_Scripts/Reproducibility/data/master_df.xlsx")
 
-#------3. Simple analysis-------
+#------3. Wind Simple analysis-------
 
 # Lets start by looking at how mean canopy height from point cloud (not interpolated) plots against average wind speed during the survey
 # To start lets look individually for each plot - and separate these out for the different plot dominant species in a panel of 16 plots
@@ -87,8 +87,8 @@ tls_int <- with(pca, center[2] - tls_slp*center[1]) # compute y-intercept
 equation <- paste("y = ", round(tls_int, 3), "+", round(tls_slp, 3), "x") # equation for printing
 
 # Compute the Lin's  correlation concordance coefficient
-ccc_result <- CCC(x, y, ci = "z-transform",conf.level = 0.95)
-ccc <- paste("CCC = ", round(ccc_result$rho.c[1], 3))
+#ccc_result <- CCC(x, y, ci = "z-transform",conf.level = 0.95)
+#ccc <- paste("CCC = ", round(ccc_result$rho.c[1], 3))
 
 MADval <- mean(abs(x-y))
 MADrel <- MADval/mean(x)*100
@@ -104,7 +104,7 @@ P1 <- ggplot(df_temp) +
   #add the statistics
   geom_text(aes(x=0.0,y=0.5),label=paste0('MAD: ',round(MADval,3)),hjust='left',size=2.0)+
   geom_text(aes(x=0.0,y=0.47),label=paste0('R2: ',round(r2val,2)),hjust='left',size=2.0)+
-  geom_text(aes(x=0.0,y=0.44),label=ccc,hjust='left', size=2.0)+
+  #geom_text(aes(x=0.0,y=0.44),label=ccc,hjust='left', size=2.0)+
   geom_text(aes(x=0.0,y=0.41),label=equation,hjust='left', size=2.0)+
   #theme(text = element_text(size=20))+
   scale_color_identity() +
@@ -132,3 +132,126 @@ plot(P1)
 }
 
 
+#-----4. Sun elevation Simple analysis -------
+
+# loop through each plot number - generating graph
+
+for (i in 1:64) {
+  
+  #Filter master df for plot number i 
+  df<- filter(master_df,plot == i)
+  
+  # Assign Average wind to X and Mean Canopy height to Y
+  
+  x <- as.vector(df$Sun_Elev_calc)
+  y <- as.vector(df$Mn_chm)
+  df_temp <- data.frame(x = x, y = y,
+                        d = densCols(x, y, colramp = colorRampPalette(rev(c('yellow','orange','turquoise4','dodgerblue4')))))#colorRampPalette(rev(rainbow(10, end = 4/6)))))
+  # Calculate Total Least Squares Regression (extracted from base-R PCA function)
+  pca <- prcomp(~x+y,df_temp)
+  tls_slp <- with(pca, rotation[2,1] / rotation[1,1]) # compute slope
+  tls_int <- with(pca, center[2] - tls_slp*center[1]) # compute y-intercept
+  equation <- paste("y = ", round(tls_int, 3), "+", round(tls_slp, 3), "x") # equation for printing
+  
+  # Compute the Lin's  correlation concordance coefficient
+ # ccc_result <- CCC(x, y, ci = "z-transform",conf.level = 0.95)
+  #ccc <- paste("CCC = ", round(ccc_result$rho.c[1], 3))
+  
+  MADval <- mean(abs(x-y))
+  MADrel <- MADval/mean(x)*100
+  lmres <- lm(y~x)
+  r2val <- summary(lmres)$r.squared
+  
+  #Plot the graph
+  
+  #(paste0("P",i,"_w")) <- ggplot(df_temp) +
+  P1 <- ggplot(df_temp) +
+    geom_smooth(aes(x, y,col='black',weight=0.01),method='lm',formula=y ~ x,se=FALSE) +
+    geom_point(aes(x, y), alpha=0.3, size = 1) +
+    #add the statistics
+    geom_text(aes(x=0.0,y=0.5),label=paste0('MAD: ',round(MADval,3)),hjust='left',size=2.0)+
+    geom_text(aes(x=0.0,y=0.47),label=paste0('R2: ',round(r2val,2)),hjust='left',size=2.0)+
+    #geom_text(aes(x=0.0,y=0.44),label=ccc,hjust='left', size=2.0)+
+    geom_text(aes(x=0.0,y=0.41),label=equation,hjust='left', size=2.0)+
+    #theme(text = element_text(size=20))+
+    scale_color_identity() +
+    theme_fancy() +
+    #add title and labels
+    ggtitle(paste0("Plot ",i,": Comparison of Canopy Height estimated from surveys with different survey sun elevations"))+
+    #theme(aspect.ratio=1)+
+    xlab('Sun Elevation (degrees)')+
+    ylab('Mean Canopy Height (m)')
+  #coord_equal(ratio=1)
+  #coord_fixed(xlim=c(0,0.8),ylim=c(0,0.8))
+  #plot(  (paste0("P",i,"_w")))
+  plot(P1)
+  ggplot2::ggsave(
+    P1,
+    # filename = "/plots/test.png",
+    filename = paste0("C:/Workspace/R_Scripts/Reproducibility/output_data/plots/Sun/P",i,"_sun_chm.jpg"),
+    width = 10,
+    height = 10,
+    units = "cm"
+  )
+  
+  
+  next
+}
+
+#------5. Is there covariance or multi-collinearity between sun. wind or cloud cover ?-----
+
+#Filter master df for plot number i 
+df<- master_df
+
+# Assign Average wind to X and Mean Canopy height to Y
+
+x <- as.vector(df$Sun_Elev_calc)
+y <- as.vector(df$Wind_Av)
+df_temp <- data.frame(x = x, y = y,
+                      d = densCols(x, y, colramp = colorRampPalette(rev(c('yellow','orange','turquoise4','dodgerblue4')))))#colorRampPalette(rev(rainbow(10, end = 4/6)))))
+# Calculate Total Least Squares Regression (extracted from base-R PCA function)
+pca <- prcomp(~x+y,df_temp)
+tls_slp <- with(pca, rotation[2,1] / rotation[1,1]) # compute slope
+tls_int <- with(pca, center[2] - tls_slp*center[1]) # compute y-intercept
+equation <- paste("y = ", round(tls_int, 3), "+", round(tls_slp, 3), "x") # equation for printing
+
+# Compute the Lin's  correlation concordance coefficient
+# ccc_result <- CCC(x, y, ci = "z-transform",conf.level = 0.95)
+#ccc <- paste("CCC = ", round(ccc_result$rho.c[1], 3))
+
+MADval <- mean(abs(x-y))
+MADrel <- MADval/mean(x)*100
+lmres <- lm(y~x)
+r2val <- summary(lmres)$r.squared
+
+#Plot the graph
+
+#(paste0("P",i,"_w")) <- ggplot(df_temp) +
+P1 <- ggplot(df_temp) +
+  geom_smooth(aes(x, y,col='black',weight=0.01),method='lm',formula=y ~ x,se=FALSE) +
+  geom_point(aes(x, y), alpha=0.3, size = 1) +
+  #add the statistics
+  geom_text(aes(x=0.0,y=0.5),label=paste0('MAD: ',round(MADval,3)),hjust='left',size=2.0)+
+  geom_text(aes(x=0.0,y=0.47),label=paste0('R2: ',round(r2val,2)),hjust='left',size=2.0)+
+  #geom_text(aes(x=0.0,y=0.44),label=ccc,hjust='left', size=2.0)+
+  geom_text(aes(x=0.0,y=0.41),label=equation,hjust='left', size=2.0)+
+  #theme(text = element_text(size=20))+
+  scale_color_identity() +
+  theme_fancy() +
+  #add title and labels
+  ggtitle(paste0("Comparison of Wind speed and sun elevation from surveys"))+
+  #theme(aspect.ratio=1)+
+  xlab('Sun Elevation (degrees)')+
+  ylab('Wind Speed (m/s)')
+#coord_equal(ratio=1)
+#coord_fixed(xlim=c(0,0.8),ylim=c(0,0.8))
+#plot(  (paste0("P",i,"_w")))
+plot(P1)
+ggplot2::ggsave(
+  P1,
+  # filename = "/plots/test.png",
+  filename = paste0("C:/Workspace/R_Scripts/Reproducibility/output_data/wind_sun.jpg"),
+  width = 10,
+  height = 10,
+  units = "cm"
+)
